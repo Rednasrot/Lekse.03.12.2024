4239ed78293926a795b938ade730c445117da8d6
Rednasrot <Rednasrot@gmail.com>
2024-12-03 14:47:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lekse.03.12.2024
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Rednasrot/Lekse.03.12.2024)
---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('Hello!');
+const julekule = document.querySelector('#julekule');
+const julekule = document.querySelector('.hidden');
+function click() {
+  const state = paragraf.style.display;
+  if (state === 'block') {
+    paragraf.style.display = 'none';
+  } else {
+    paragraf.style.display = 'none';
+  }
+  paragraf.style.display = 'block';
+}
+
+julekule.addEventListener('click', click);
